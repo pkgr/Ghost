eded12a8a77cfce756f07768e67febf7abae4c85
Cyril Rohr <cyril.rohr@gmail.com>
2014-03-25 19:48:09 +0000

    Attempt to make Ghost configurable via environment variables.

---
+++ packaging/after.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Compile assets. This requires the full development stack...
+HOME=$(pwd)
+source .profile.d/nodejs.sh
+npm install
+git init
+./node_modules/.bin/grunt init
+./node_modules/.bin/grunt prod
+
+# Setup a proper config.js file, taking settings from environment variables.
+cat > config.js <<CONFIG
+var path = require('path'),
+    url = require('url'),
+    config;
+
+var ghost_port = process.env.PORT,
+    ghost_url = process.env.GHOST_URL || ("http://127.0.0.1:" + port),
+    pg_url = url.parse(process.env.DATABASE_URL);
+
+
+config = {
+  production: {
+    url: ghost_url,
+    database: {
+        client: 'pg',
+        connection: {
+            host     : pg_url.host,
+            port     : pg_url.port,
+            user     : pg_url.auth.split(":")[0],
+            password : pg_url.auth.split(":")[1],
+            database : pg_url.pathname,
+            charset  : 'utf8'
+        }
+    },
+    server: {
+        host: '127.0.0.1',
+        port: ghost_port
+    },
+    logging: false
+  }
+}
+CONFIG
